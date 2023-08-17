8690f6bf30541830af5a4c6168da1ed8aa8aea62
jpadwor1 <128070765+jpadwor1@users.noreply.github.com>
2023-08-17 09:07:53 -0700

    Initial commit

---
+++ Challenge 2/Instructions.py
@@ -0,0 +1,37 @@
+"""
+Challenge 1: Username Generator
+Tier: 1-Beginner
+
+Objective:
+Develop a program that takes user input and combines them using an f-string to create a unique and personalized username.
+
+Description:
+Usernames are essential in the digital age, whether for social media, gaming, or professional platforms. In this challenge, you'll be creating a program that asks users about their childhood street name and their pet's name. The goal is to combine these names using Python's f-string to generate a unique username that's both personal and memorable.
+
+Features:
+
+Gather User Input:
+
+Ask the user for the name of the street they grew up on.
+Ask the user for the name of their pet.
+Combine Names using f-string:
+
+Use an f-string to combine the street name and the pet's name.
+Display Username:
+
+Show the user their generated username.
+Sample Flow:
+
+User is prompted: "What was the name of the street you grew up on?"
+User enters: "Maplewood"
+User is prompted: "What was your pet's name?"
+User enters: "Whiskers"
+The program generates the username: "Maplewood_Whiskers" using an f-string and displays it to the user.
+Constraints:
+
+Ensure that the user's input for the street name and pet's name is not empty. If it is, prompt them again.
+Bonus Features:
+
+Allow the user to choose a separator (like a hyphen, underscore, or period) to use between the names.
+Add a feature where the program suggests multiple username variations for the user to choose from.
+"""
---
+++ Challenge 2/Solution.py
@@ -0,0 +1,16 @@
+"""
+Challenge 1: Username Generator
+Tier: 1-Beginner
+
+Objective:
+Develop a program that takes user input and combines them using an f-string to create a unique and personalized username.
+
+"""
+user_street = input("What street did you grow up on? Omit St.,Rd., etc.")
+
+
+user_pet_name = input("What is the name of your favorite pet?")
+
+
+print(f"Your username is {user_street}_{user_pet_name}.")
+
